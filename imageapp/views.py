85557abbf75032429250a19c5e4963cab7f54e0d
knrd <jvdswi@jadamspam.pl>
2016-03-10 11:43:18 +0100

    handling views

--- imageapp/views.py
+++ imageapp/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Image, ImageForm
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 def main_form(request):
     if request.method == 'POST':
@@ -18,6 +21,22 @@ def main_form(request):
         # save changes only when new image is provided
         if form.is_valid() and request.FILES:
             form.save(dname=objname)
+
+            info = {}
+            info['direct_link'] = settings.MEDIA_URL + form.cleaned_data['name'] + "." + form.cleaned_data['image'].name.split('.')[-1]
+            info['detail_link'] = reverse('imageapp:detail', args=(form.cleaned_data['name'],))
+            if obj:
+                info['message'] = form.cleaned_data['name'] + " image changed"
+            else:
+                info['message'] = form.cleaned_data['name'] + " image uploaded"
+
+            return render(request, 'imageapp/success.html', info)
+            # return HttpResponseRedirect('/success/url/')
     else:
         form = ImageForm()
     return render(request, 'imageapp/mainform.html', {'formset': form})
+
+class ImageDetailView(generic.DetailView):
+    template_name = 'imageapp/detail.html'
+    model = Image
+    slug_field = 'name'
